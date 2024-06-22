64fc5c06bf30c98fa884c5dafdd64eedc6e0db75
Kevin Grant <anmoron11@gmail.com>
2024-06-21 23:01:43 -0400

    did stuff.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 
 function App() {
+  console.log(process.env.REACT_APP_API_URL);
+
   return (
     <React.Fragment>
       <Header />
